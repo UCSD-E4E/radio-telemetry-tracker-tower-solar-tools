cba96ac8552f3a65700eddd8af39f6dd7e2e6f50
Vasil Bogdev <97698841+vbogdev@users.noreply.github.com>
2024-04-08 09:38:53 -0700

    Add files via upload
    added file used for calculating battery charge based off of solar panel output

---
+++ tower_battery/charge_sim.py
@@ -0,0 +1,50 @@
+import csv
+
+
+
+leadAcidBatterySize = 65 # in amp hours
+energyStored = leadAcidBatterySize * 12.1 # average voltage from 100% to 50% * AH of battery
+maxEnergyStored = energyStored # assuming battery starts with maximum energy
+chargeControllerEfficiency = 0.95 # from data sheet
+leadAcidChargingEfficiency = 0.85 # normal for lead acid batteries
+extraOverhead = 1.05 # to account for extra loss from wires
+systemLosses = chargeControllerEfficiency * leadAcidChargingEfficiency
+timeUnit = 5 / 60 # 5 minutes, in hours
+
+energyConsumed = 12.1 *  1 # tower consumes about 12.1 Watts
+
+finalData = []
+ifNeedsLargerBattery = 0
+count = 0
+
+with open('data.csv', mode = 'r') as file:
+    csvFile = csv.reader(file)
+    for line in csvFile:
+        # calculate energy change over timeUnit period
+        energyStored = energyStored + float(line[0]) * timeUnit * systemLosses - energyConsumed * timeUnit * extraOverhead
+
+        # handle if battery is empty, full, or could be damaged
+        if energyStored > maxEnergyStored:
+            energyStored = maxEnergyStored
+        if energyStored < 0:
+            energyStored = 0
+        finalData.append((energyStored / maxEnergyStored))
+        count = count + 1
+        if energyStored < (maxEnergyStored * 0.4): # check if this could damage battery
+            ifNeedsLargerBattery = 1
+
+with open('finalData.csv', 'w', newline='\n') as dataFile:
+    writer = csv.writer(dataFile)
+    for dat in finalData:
+        row = []
+        row.append(dat)
+        writer.writerow(row)
+
+if ifNeedsLargerBattery == 1:
+    print('Larger battery required')
+else:
+    print('Battery is sufficient')
+
+print('Wrote ')
+print(count)
+print(' data points')
